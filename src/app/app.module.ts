3bfd1b90999f5b660d5cd570f1ab077986b37a55
YuriKomesuAugusto <komesu.yuri.a@gmail.com>
2022-02-22 12:04:50 -0300

    improving LiveListComponent and adding @angular/flex-layout dependency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -10,6 +11,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
 import { LiveListComponent } from './views/home/live-list/live-list.component';
+
 
 @NgModule({
   declarations: [
@@ -24,7 +26,8 @@ import { LiveListComponent } from './views/home/live-list/live-list.component';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatTabsModule
+    MatTabsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
